21db8d8372adb13066241a5c65587f18cedb2d43
rekamanoharan <rekamano15@gmail.com>
2023-06-11 00:13:21 +0530

    callbac hell using timer

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const greet = (message = "", cb = (d) => {}) => {
-    console.log(message);
-    cb(message);
-  };
-  
-  greet(10, () => {
-    greet(9, () => {
-      greet(8, () => {
-        greet(7, () => {
-          greet(6, () => {
-            greet(5, () => {
-              greet(4, () => {
-                greet(3, () => {
-                  greet(2, () => {
-                    greet(1, () => {
-                      greet("Happy Independence Day");
-                    });
+
+
+$( document ).ready(function() {
+  document.querySelector("#callback-btn")
+.addEventListener("click", function() {
+    const greet = (message = "", cb = (d) => {}) => {
+  console.log(message);
+  $('#printNumber').append(`<h4>${message}</h4>`);
+ setTimeout(()=>{
+  cb(message);
+  },2000);
+};
+
+greet(10, () => {
+  greet(9, () => {
+    greet(8, () => {
+      greet(7, () => {
+        greet(6, () => {
+          greet(5, () => {
+            greet(4, () => {
+              greet(3, () => {
+                greet(2, () => {
+                  greet(1, () => {
+                    greet("Happy Independence Day");
                   });
                 });
               });
@@ -24,3 +31,6 @@ const greet = (message = "", cb = (d) => {}) => {
       });
     });
   });
+});
+});
+});
